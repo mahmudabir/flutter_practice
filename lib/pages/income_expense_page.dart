1f50e125dda6256f618e713a63cb32eda58b64b2
Abir <amabirmahmud@gmail.com>
2025-05-06 15:34:13 +0600

    Update income_expense_page.dart

--- lib/pages/income_expense_page.dart
+++ lib/pages/income_expense_page.dart
@@ -11,98 +11,134 @@ class IncomeExpensePage extends StatefulWidget {
 }
 
 class _IncomeExpensePageState extends State<IncomeExpensePage> {
-  double balance = 0.0;
-  List<Map<String, dynamic>> expenses = [];
+  double _balance = 0.0;
+  final List<Map<String, dynamic>> _expenses = [];
 
-  final TextEditingController incomeController = TextEditingController();
-  final TextEditingController expenseAmountController = TextEditingController();
-  final TextEditingController expenseDescController = TextEditingController();
+  final TextEditingController _incomeController = TextEditingController();
+  final TextEditingController _expenseAmountController =
+      TextEditingController();
+  final TextEditingController _expenseDescController = TextEditingController();
 
   void _addIncome() {
-    final amount = double.tryParse(incomeController.text);
+    final amount = double.tryParse(_incomeController.text);
     if (amount != null && amount > 0) {
       setState(() {
-        balance += amount;
+        _balance += amount;
       });
-      incomeController.clear();
+      _incomeController.clear();
     }
   }
 
   void _addExpense() {
-    final amount = double.tryParse(expenseAmountController.text);
-    final desc = expenseDescController.text;
+    final amount = double.tryParse(_expenseAmountController.text);
+    final desc = _expenseDescController.text;
 
     if (amount != null && amount > 0 && desc.isNotEmpty) {
       setState(() {
-        balance -= amount;
-        expenses.add({'amount': amount, 'description': desc});
+        _balance -= amount;
+        _expenses.add({'amount': amount, 'description': desc});
       });
-      expenseAmountController.clear();
-      expenseDescController.clear();
+      _expenseAmountController.clear();
+      _expenseDescController.clear();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Invalid amount or description'),
+          showCloseIcon: true,
+          shape: RoundedRectangleBorder(),
+        ),
+      );
     }
   }
 
   @override
   void dispose() {
-    incomeController.dispose();
-    expenseAmountController.dispose();
-    expenseDescController.dispose();
+    _incomeController.dispose();
+    _expenseAmountController.dispose();
+    _expenseDescController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Income & Expense Tracker')),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Income & Expense Tracker'),
+      ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              'Current Balance: \$${balance.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: incomeController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Add Income',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: _addIncome,
-                ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double maxWidth = constraints.maxWidth;
+          double contentWidth = maxWidth > 800 ? 600 : double.infinity;
+
+          return Center(
+            child: Container(
+              width: contentWidth,
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Current Balance: \$${_balance.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _incomeController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Add Income',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: _addIncome,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  TextField(
+                    controller: _expenseAmountController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: 'Expense Amount'),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _expenseDescController,
+                    decoration: InputDecoration(
+                      labelText: 'Expense Description',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple, // background color
+                      foregroundColor: Colors.white, // text color
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                    onPressed: _addExpense,
+                    child: const Text('Add Expense'),
+                  ),
+                  SizedBox(height: 20),
+                  Text('Expenses:', style: TextStyle(fontSize: 18)),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: _expenses.length,
+                      itemBuilder: (context, index) {
+                        final expense = _expenses[index];
+                        return ListTile(
+                          leading: const Icon(Icons.money_off),
+                          title: Text('\$${expense['amount']}'),
+                          subtitle: Text(expense['description']),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 20),
-            TextField(
-              controller: expenseAmountController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Expense Amount'),
-            ),
-            TextField(
-              controller: expenseDescController,
-              decoration: InputDecoration(labelText: 'Expense Description'),
-            ),
-            ElevatedButton(onPressed: _addExpense, child: Text('Add Expense')),
-            SizedBox(height: 20),
-            Text('Expenses:', style: TextStyle(fontSize: 18)),
-            Expanded(
-              child: ListView.builder(
-                itemCount: expenses.length,
-                itemBuilder: (context, index) {
-                  final expense = expenses[index];
-                  return ListTile(
-                    leading: Icon(Icons.money_off),
-                    title: Text('\$${expense['amount']}'),
-                    subtitle: Text(expense['description']),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
